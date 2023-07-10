bb6a9acf490bf6511b70c93d34138e8067df0e1c
Alexandru Badea <badeaalexsandu@gmail.com>
2023-07-10 23:02:14 +0300

    Created the model and controller

---
+++ ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        private static List<ToDoItem> _listItems { get; set; } = new List<ToDoItem>();
+
+
+
+        [HttpGet("getAll", Name ="GetAllToDoItems")]
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return _listItems;
+        }
+
+        [HttpGet("getbyid/{id}", Name = "GetToDoItem")]
+        public ActionResult<ToDoItem> GetItemById(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateItem(ToDoItem item)
+        {
+            _listItems.Add(item);
+            return CreatedAtRoute("GetToDoItem", new { id = item.Id}, item);
+        }
+
+        [HttpPut("update/{id}")]
+        public IActionResult Update(int id, ToDoItem item)
+        {
+            var filter = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            filter.Title = item.Title;
+            filter.Description = item.Description;
+            filter.isCompleted = item.isCompleted;
+
+            return CreatedAtRoute("GetToDoItem", new { i = item.Id }, item); ;
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _listItems.Remove(item);
+            return NoContent();
+        }
+    
+    }
+}
---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [DefaultValue(false)]
+        public bool isCompleted { get; set; }
+
+    }
+}
